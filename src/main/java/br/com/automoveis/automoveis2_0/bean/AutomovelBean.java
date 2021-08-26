f814ad72fdfbf6a4d4d2af794384a4632e47156b
andrew.alves <60-andrew.alves@users.noreply.gitlab.boavistanet.com.br>
2021-08-26 15:35:56 -0300

    ajustes nas libs

--- src/main/java/br/com/automoveis/automoveis2_0/bean/AutomovelBean.java
+++ src/main/java/br/com/automoveis/automoveis2_0/bean/AutomovelBean.java
@@ -1,12 +1,13 @@
 package br.com.automoveis.automoveis2_0.bean;
 
-import br.com.automoveis.automoveis2_0.entities.Automovel;
-import br.com.automoveis.automoveis2_0.util.JPAUtil;
+import java.util.List;
 
 import javax.annotation.ManagedBean;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-import java.util.List;
+
+import br.com.automoveis.automoveis2_0.entities.Automovel;
+import br.com.automoveis.automoveis2_0.util.JPAUtil;
 
 @ManagedBean
 public class AutomovelBean {
